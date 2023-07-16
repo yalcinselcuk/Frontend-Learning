402510aa50f17bb1f0c2b9beb28a34f8a6ecd10c
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-07-17 00:15:43 +0300

    jsx işlendi

--- React/myfirstproject/src/App.js
+++ React/myfirstproject/src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  let students = 40;
+  const nameAndSurname = "yalçın selçuk";
+  let dogruMu = false;
+  let sonuc = (dogruMu ===true) ? "doğru imiş" : "yanlış imiş";
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        Yalçın
-      </header>
+    <div>
+      <h1>isim, soyisim : {nameAndSurname}</h1>
+      <p>Yalçın</p>
+      <p>Öğrenci Sayısı : {students}</p>
+      <p>Bool İfade Sonucu : {sonuc}</p>
     </div>
   );
 }
