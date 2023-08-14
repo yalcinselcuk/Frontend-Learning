a960269b1b0760f349a47ca32b576bdc8b8e3c72
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-08-14 16:36:05 +0300

    sayaç eklendi

--- React/useref_used/src/App.js
+++ React/useref_used/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter';
+import Stopwatch from './StopWatch';
 
 function App() {
   return (
     <div className="App">
       <Counter/>
+      <Stopwatch/>
     </div>
   );
 }
